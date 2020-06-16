761c8f7bf788177323158f61b32c5e20036b675c
Cooper Weiser <cooper@Coopers-MacBook-Pro.local>
2020-06-16 02:51:14 -0700

    added users service, auth service, and users controller

---
+++ src/users/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = new User({ ...createUserDto });
+  }
+}
